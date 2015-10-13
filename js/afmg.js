8ffc4221bbc14432adf4866740ee5110906fe92c
andreiamfg <andreia.mfg@gmail.com>
2015-10-12 19:50:02 -0700

    show/hide nav with scroll

--- js/afmg.js
+++ js/afmg.js
@@ -3,6 +3,35 @@ $(document).ready( function()
   $('#nav-contact').on('touchstart click', function() {goToDiv("#main-contact")});
   $('#nav-about').on('touchstart click', function() {goToDiv("#main-about")});
   $('#nav-work').on('touchstart click', function() {goToDiv("#main-featured-work")});
+
+  var lastScrollTop = 0,
+    delta = 100;
+
+  $(function()
+  {
+
+    $(window).scroll(function(event)
+    {
+      var st = $(this).scrollTop();
+
+      if (Math.abs(lastScrollTop - st) <= delta)
+        return;
+
+      if (st > lastScrollTop)
+      {
+        //Hide nav
+        $("nav").animate({top:-100}, 10);
+
+      }
+      else
+      {
+        // Show nav
+        $("nav").animate({top:0}, 10);
+      }
+      lastScrollTop = st;
+    });
+  });
+
 });
 
 function goToDiv(target)
