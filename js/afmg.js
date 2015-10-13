77ab5d6192edd1e6cb89127f825904fad30b82f0
andreiamfg <andreia.mfg@gmail.com>
2015-10-12 18:05:39 -0700

    slide to div with touch

--- js/afmg.js
+++ js/afmg.js
@@ -1,6 +1,8 @@
 $(document).ready( function()
 {
-
+  $('#nav-contact').on('click touchstart', function() {goToDiv("#main-contact")});
+  $('#nav-about').on('click touchstart', function() {goToDiv("#main-about")});
+  $('#nav-work').on('click touchstart', function() {goToDiv("#main-featured-work")});
 });
 
 function goToDiv(target)
